aab97df931176e98f879f998d1f980841e86124b
Brandon Seamer <brandonseamer@gmail.com>
2018-04-25 12:32:53 -0500

    x

--- Computer Guessing Number Game/Computer Guessing Number Game/Source.cpp
+++ Computer Guessing Number Game/Computer Guessing Number Game/Source.cpp
@@ -5,25 +5,32 @@
 #include<time.h>
 #include<iostream>
 #include<string>
+#include<Windows.h>
 int main()
 {
-	char userInput;
+	int userInput;
 	int rangeMax;
 	int rangeMin;
 	int compGuess;
 	int attempts;
-	
+	int time;
 	
 	std::cout << "What is the maximum of your range? >>> \n";
 	std::cin >> rangeMax;
 	std::cout << "What is the minimum of your range? >>> \n";
 	std::cin >> rangeMin;
-guessAgain:
-	srand(time(0));
-	compGuess = rand() % rangeMax + rangeMin;
-	std::cout << "Is " << compGuess << " your number? ('Y' or 'N') >>> \n";
+	std::cout << "What number would you like me to guess? >>> \n";
 	std::cin >> userInput;
-	if (!(toupper(userInput) == 'Y'))
+guessAgain:
+	srand(GetTickCount());
+	time = GetTickCount();
+	if (time == GetTickCount())
+	goto guessAgain;
+	compGuess = rand() % rangeMax + rangeMin;
+	attempts++;
+
+		std::cout << "I have taken " << attempts << " attempts so far. \n";
+	if (userInput != compGuess)
 		goto guessAgain;
 	else
 		std::cout << "Yay I win! \n";
--- Final Project/Final Project/Source.cpp
+++ Final Project/Final Project/Source.cpp
@@ -10,19 +10,14 @@
 #include<Windows.h>
 #include<time.h>
 #include<fstream>
+
 //ofstream is writing to a file
 //ifstream is reading from a file
 
-
 //Declare Global Variables
-std::string characterName;
-std::string reasonForDebt;
-std::string winMessage;
-std::string loseMessage;
-
-double cash;
-
 int stats[7];
+
+std::string characterName; 
 
 int strength;
 int agility;
@@ -32,16 +27,40 @@ int luck;
 int endurance;
 int perception;
 
-
-
-int rollCash()
+void writeStats()
 {
-	return 0;
-}
+	std::ofstream stats("stats.txt", std::ios::beg);
+	stats << "Strength: " << strength << ". \n";
+	stats << "Perception: " << perception << ". \n";
+	stats << "Endurance: " << endurance << ". \n";
+	stats << "Charisma: " << charisma << ". \n";
+	stats << "Intelligence: " << intelligence << ". \n";
+	stats << "Agility: " << agility << ". \n";
+	stats << "Luck: " << luck << ". \n";
 
+
+	stats.close();
+}
+void readStats()
+{
+	std::cout << characterName << "'s stats are: \n";
+	std::string line;
+	std::ifstream stats("stats.txt", std::ios::beg);
+	while (getline(stats, line))
+	{
+		std::cout << line << "\n";
+	}
+	stats.close();
+
+}
 int rollStats()
 {
+	int userInput;
+	setup:
 	srand(GetTickCount());
+	int time = GetTickCount();
+	if (time == GetTickCount())
+		goto setup;
 	for (int x = 6; x > -1; x--)
 	{
 
@@ -50,29 +69,25 @@ int rollStats()
 			stats[x] = stats[x] + rand() % 6 + 1;
 		}
 	}
-	strength = stats[0];
-	agility = stats[1];
-	intelligence = stats[2];
-	charisma = stats[3];
-	luck = stats[4];
-	endurance = stats[5];
-	perception = stats[6];
-	return 0;
-}
+	std::cout << "Your stat options are: 1 = " << stats[0] << "\n 2 = " << stats[1] << "\n 3 = " << stats[2] << "\n 4 = " << stats[3] << "\n 5 = " << stats[4] << "\n 6 = " << stats[5] << "\n 7 = " << stats[6];
+	std::cout << "What would you like your strength to be? Option (1,2,3,4,5,6, or 7 from above) Dont select the same option twice. \n";
+	std::cin >> userInput;
+	strength = stats[userInput];
+	std::cout << "What would you like your perception to be? Option (1,2,3,4,5,6, or 7 from above) Dont select the same option twice. \n";
+	std::cin >> userInput;
+	perception = stats[userInput];
 
-int characterSetUp()
-{
 	return 0;
 }
 
 int main()
 {
+	std::cout << "Welcome to Brandon's Adventure!! \n\n\nWhat is your adventurer's name? \n";
+	std::cin >> characterName;
 	rollStats();
-	std::cout << "Strength: " << strength << ". \n";
-	std::cout << "Agility: " << agility << ". \n";
-	std::cout << "Intelligence: " << intelligence << ". \n";
-	std::cout << "Charisma: " << charisma << ". \n";
-	std::cout << "Luck: " << luck << ". \n";
+	writeStats();
+	readStats();
+
 	system("pause");
 	return 0;
 }
